2908565ac7fbd0a50d94661b1d43c51dd0e2dbde
headpiece747 <8082268+headpiece747@users.noreply.github.com>
2026-03-30 20:13:59 -0400

    Use 3-part version (1.0.0) to match ElmorLabs widget style

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,33 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("PictureViewerWidget")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Click to cycle through pictures in a folder.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PictureViewerWidget")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("76ba707f-ccb6-4b68-886d-0248d8c708f6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// 3-part version — matches ElmorLabs widget style (v1.0.0 not v1.0.0.0)
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
